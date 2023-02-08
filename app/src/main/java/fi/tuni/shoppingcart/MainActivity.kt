c390f1df7dff4cc12a82b015ec7b5f82930f8eb7
seppohyvarinen <72986742+seppohyvarinen@users.noreply.github.com>
2023-02-08 13:46:05 +0200

    Hide action bar

--- app/src/main/java/fi/tuni/shoppingcart/MainActivity.kt
+++ app/src/main/java/fi/tuni/shoppingcart/MainActivity.kt
@@ -1,15 +1,17 @@
 package fi.tuni.shoppingcart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import fi.tuni.myapplication.R
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 lateinit var header : TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
         setContentView(R.layout.activity_main)
         header = findViewById(R.id.header)
     }
